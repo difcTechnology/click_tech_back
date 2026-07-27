c14bf602da0f7ed3541c903d2fa739df4f38b3a9
Luis2612 <luisimba2612@gmail.com>
2026-07-27 17:55:10 -0500

    creacion de plantilla spring boot

---
+++ src/main/java/com/clicktech/backend/ClickTechApplication.java
@@ -0,0 +1,13 @@
+package com.clicktech.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickTechApplication.class, args);
+    }
+
+}
